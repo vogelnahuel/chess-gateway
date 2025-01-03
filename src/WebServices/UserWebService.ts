b22e936a46f3b4f8c598eee6eb0f88366b5bc189
vogelnahuel <vogelnahuel@gmail.com>
2025-01-03 16:10:32 -0300

    agregando nuevos protos

--- src/WebServices/UserWebService.ts
+++ src/WebServices/UserWebService.ts
@@ -3,7 +3,6 @@ import { GrpcWebServices } from './GrpcWebService';
 
 export class UserWebService extends GrpcWebServices {
     async createUser(data: CreateUserDto): Promise<any> {
-        console.log('data', data);
         this.createGrpcClientWebService('User.proto', 'UserService', process.env.AUTH_GRPC_URL || 'localhost:50051');
         return this.call<any, typeof data>('CreateUser', data);
     }
