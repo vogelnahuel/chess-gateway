da118933b822cdf9808a028f0224973926f9b384
vogelnahuel <vogelnahuel@gmail.com>
2025-01-27 20:57:49 -0300

    borrando console log

--- src/WebServices/UserWebService.ts
+++ src/WebServices/UserWebService.ts
@@ -42,7 +42,6 @@ export class UserWebService extends GrpcWebServices {
     async getUserById(id: number): Promise<any> {
         let result = null;
         try {
-            console.log('id', id);
             this.createGrpcClientWebService('User.proto', 'UserService', process.env.AUTH_GRPC_URL || 'localhost:50051');
             result = this.call<any, { id: number }>('GetUserById', { id });
         } catch (error) {
